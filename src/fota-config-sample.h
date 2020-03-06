41ac4d59583989e8e0a327442746dd01d973252c
Andreas Alptun <andreas.alptun@gmail.com>
2020-03-06 13:41:12 +0100

    Added fota-config-sample.h

--- src/fota-config-sample.h
+++ src/fota-config-sample.h
@@ -20,36 +20,19 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
-#ifndef FOTA_H
-#define FOTA_H
-#if __has_include("fota-config.h")
-#include "fota-config.h"
-#else
-#include "fota-config-sample.h"
-#endif
-#include "buffer.h"
+#ifndef FOTA_CONFIG_H
+#define FOTA_CONFIG_H
 
-#define ALIGN16(v) (((v)+15)&(~15))
+#warning "Sample config! Copy fota-config-sample.h to fota-config.h and use project specific keys"
 
-typedef unsigned char uuid_t[16];
-typedef unsigned char rsa_sign_t[RSA_KEY_BITSIZE/8];
-typedef unsigned char rsa_cipher_t[RSA_KEY_BITSIZE/8];
-typedef unsigned char sha_hash_t[32];
-typedef unsigned char aes_key_t[AES_KEY_BITSIZE/8];
-typedef unsigned char aes_iv_t[16];
+#define MODEL_ID_MK1 "mk1"
+#define MODEL_KEY_MK1 {0x51,0x92,0x19,0x26,0x94,0x31,0x50,0x64,0x68,0xc1,0xf8,0x99,0x59,0x5a,0xfe,0x29}
+#define MODEL_KEYS {{MODEL_ID_MK1, MODEL_KEY_MK1}}
+#define FIREBASE_PROJECT "xxx-fota"
 
-// Must be called before any of the function below
-void fota_init();
+#define RSA_KEY_BITSIZE 1024
+#define RSA_KEY_MODULO "e77adc0850e71526aa47dca5e281e24c68d0ed81bf09c84dd1788abffe7d9343ba273e91de9839ed6d55ed9a3432fb69c623773fa6043ff39d45ab61d4ab1270449b630964f21576cb448f8f6b0c608bb2b61faf8c6156adec5af21f536821cfa183ef15f513faaafe01b9087d74077e861c557e4fe0c2a45273f1fdc0261817"
+#define RSA_KEY_PUBLIC_EXP "010001"
+#define AES_KEY_BITSIZE 128
 
-// Returns a static model id string, do not free
-const char* fota_model_id();
-
-// Generates a request token hex string for downloading the firmware package from the server
-// returned buffer should be free'd
-char* fota_request_token();
-
-// Verify package, unwrap it and return the firmware binary
-// returned buffer should be free'd
-buffer_t* fota_verify_package(buffer_t* fwpk_enc2_buf);
-
-#endif //FOTA_H
+#endif //FOTA_CONFIG_H
