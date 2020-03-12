770fd9ffec01ffd28a4a317196b9df9ac6a8af53
Andreas Alptun <andreas.alptun@gmail.com>
2020-03-12 14:28:40 +0100

    Update fota-client to use new API with empty integration funcs

--- src/fota-client.c
+++ src/fota-client.c
@@ -21,15 +21,30 @@
 // SOFTWARE.
 
 #include "fota.h"
-#include <stddef.h>
 
-// TODO update!
+void fotai_read_storage_page(uint8_t* buf, int page) {}
+void fotai_write_firmware_page(uint8_t* buf, int page, int len) {}
+
+void fotai_get_unique_key(fota_aes_key_t unique_key) {}
+void fotai_get_public_key(fota_rsa_key_t public_key, int type) {}
+
+void fotai_generate_random(uint8_t* buf, int len) {}
+
+void fotai_aes_decrypt_init(fota_aes_key_t key, void** ctx) {}
+void fotai_aes_decrypt_block(uint8_t* in, uint8_t* out, int len, fota_aes_iv_t iv, void* ctx) {}
+void fotai_aes_decrypt_free(void* ctx) {}
+
 int main() {
-  if(!fota_request_token())
+  
+  fota_token_t token;
+  if(!fota_request_token(token))
     return 1;
 
-  // buffer_t* buf = buf_alloc(1024);
   if(!fota_verify_package()) {
+    return 1;
+  }
+
+  if(!fota_install_package()) {
     return 1;
   }
 
